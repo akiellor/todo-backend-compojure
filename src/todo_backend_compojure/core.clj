4ba020593c781a500bbd62dc694b1e6c9aa59975
Andrew Kiellor <akiellor@gmail.com>
2014-07-27 23:36:34 -0700

    Simplified storage structure.

--- src/todo_backend_compojure/core.clj
+++ src/todo_backend_compojure/core.clj
@@ -2,7 +2,7 @@
   (:require [compojure.core :refer :all]
             [clojure.data.json :as json]))
 
-(def store (atom {:todos {}}))
+(def todos (atom {}))
 
 (defn uuid [] (str (java.util.UUID/randomUUID)))
 
@@ -10,33 +10,32 @@
   (json/read-str (slurp body) :key-fn keyword))
 
 (defn get-todo [id]
-  (let [todo (get-in @store [:todos id])]
+  (let [todo (get-in @todos [id])]
     (if (nil? todo)
       nil 
       (assoc todo :url (str "http://localhost:8080/todos/" id)))))
 
 (defn create [todo]
   (let [id (uuid) new-todo (merge todo {:id id :completed false})]
-    (swap! store (fn [state]
-                   (let [{todos :todos} state]
-                     {:todos (assoc todos id new-todo)})))
+    (swap! todos (fn [state]
+                     (assoc state id new-todo)))
     (get-todo id)))
 
 (defn get-all []
-  (let [ids (keys (:todos @store))]
+  (let [ids (keys @todos)]
     (map get-todo ids)))
 
 (defn patch-todo [id body]
-  (swap! store (fn [state]
-                 (assoc-in state [:todos id] (merge (get-todo id) body))))
+  (swap! todos (fn [state]
+                 (merge state {id (merge (get-todo id) body)})))
   (get-todo id))
 
 (defn delete []
-  (swap! store assoc :todos {}))
+  (swap! todos {}))
 
 (defn delete-todo [id]
-  (swap! store (fn [state]
-                 {:id (:id state) :todos (dissoc (:todos state) id)})))
+  (swap! todos (fn [state]
+                 (dissoc state id))))
 
 (defn res->created [result]
   {:status 201
